bd80138bdd472816d342a676447b610a01d7da46
Your Name <you@example.com>
2020-04-14 17:52:21 -0400

    testing hp for linear

--- configs/linear_maze_config.py
+++ configs/linear_maze_config.py
@@ -26,7 +26,7 @@ volatile_agent_config = {
     "vanilla": {
         "planning_depth": [0],
         "replay_capacity": [0],
-        "lr": np.linspace(0.1, 1.0, 10),
+        "lr": np.linspace(0.01, 0.1, 10),
         "lr_p": [0],
         "lr_m": [0]
     },
