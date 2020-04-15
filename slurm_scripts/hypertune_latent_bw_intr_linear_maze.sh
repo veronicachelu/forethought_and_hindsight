0f6d08eae98d11f900fe2cd663abf5f1d93f8f77
Your Name <you@example.com>
2020-04-15 12:02:19 -0400

    testing hp for linear

--- slurm_scripts/hypertune_latent_bw_intr_linear_maze.sh
+++ slurm_scripts/hypertune_latent_bw_intr_linear_maze.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=hypertune_MG_maze
-#SBATCH --output=hypertune_MG_maze_output.txt
-#SBATCH --error=hypertune_MG_maze_error.txt
+#SBATCH --output=./outputs/hypertune_MG_maze_output.txt
+#SBATCH --error=./errors/hypertune_MG_maze_error.txt
 #SBATCH --ntasks=1
 #SBATCH --time=10:00
 #SBATCH --mem=100Gb
