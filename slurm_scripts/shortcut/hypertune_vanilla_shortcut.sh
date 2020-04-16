b70a31a47db274e3081032305e7aea9318c39dcb
Your Name <you@example.com>
2020-04-16 10:23:57 -0400

    small fixes

--- slurm_scripts/shortcut/hypertune_vanilla_shortcut.sh
+++ slurm_scripts/shortcut/hypertune_vanilla_shortcut.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=hypertune__vanilla__repeat
-#SBATCH --output=./outputs/hypertune__vanilla__repeat__output.txt
-#SBATCH --error=./errors/hypertune__vanilla__repeat__error.txt
+#SBATCH --job-name=hypertune__vanilla__shortcut
+#SBATCH --output=./outputs/hypertune__vanilla__shortcut__output.txt
+#SBATCH --error=./errors/hypertune__vanilla__shortcut__error.txt
 #SBATCH --ntasks=1
 #SBATCH --time=10000
 #SBATCH --mem=100Gb
 
 source ~/.bashrc
 cd ~/jax_models
-python hyper_tune.py --agent=vanilla --env=repeat
+python hyper_tune.py --agent=vanilla --env=shortcut
