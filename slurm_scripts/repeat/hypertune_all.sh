58f4102f2d5caac4866f7f3aec6c498d71b5d75d
Your Name <you@example.com>
2020-04-16 19:52:17 -0400

    small fixes

---
+++ slurm_scripts/repeat/hypertune_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=hypertune__all__shortcut
+#SBATCH --output=./outputs/hypertune__all__shortcut__output.txt
+#SBATCH --error=./errors/hypertune__all__shortcut__error.txt
+#SBATCH --ntasks=1
+#SBATCH --time=10000
+#SBATCH --mem=100Gb
+
+source ~/.bashrc
+cd ~/jax_models
+python hyper_tune.py --agent=bw --env=repeat
+python hyper_tune.py --agent=fw --env=repeat
+python hyper_tune.py --agent=bw_fw --env=repeat
+python hyper_tune.py --agent=fw_rnd --env=repeat
+python hyper_tune.py --agent=fw_pri --env=repeat
+python hyper_tune.py --agent=bw_fw_PWMA --env=repeat
+python hyper_tune.py --agent=bw_fw_MG --env=repeat
