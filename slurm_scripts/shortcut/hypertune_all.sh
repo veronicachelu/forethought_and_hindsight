10af7beba2dff1c05c846d46d09d4f2b6aec12c1
Your Name <you@example.com>
2020-04-16 19:22:53 -0400

    small fixes

---
+++ slurm_scripts/shortcut/hypertune_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=hypertune__all__shortcut
+#SBATCH --output=./outputs/hypertune__all__shortcut__output.txt
+#SBATCH --error=./errors/hypertune__all__shortcut__error.txt
+#SBATCH --ntasks=1
+#SBATCH --time=10000
+#SBATCH --mem=100Gb
+
+source ~/.bashrc
+cd ~/jax_models
+python hyper_tune.py --agent=bw --env=shortcut
+python hyper_tune.py --agent=fw --env=shortcut
+python hyper_tune.py --agent=bw_fw --env=shortcut
+python hyper_tune.py --agent=fw_rnd --env=shortcut
+python hyper_tune.py --agent=fw_pri --env=shortcut
+python hyper_tune.py --agent=bw_fw_PWMA --env=shortcut
+python hyper_tune.py --agent=bw_fw_MG --env=shortcut
--- slurm_scripts/shortcut/hypertune_bw_fw_MG_shortcut.sh
+++ slurm_scripts/shortcut/hypertune_bw_fw_MG_shortcut.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=hypertune__bw_fw_MG__shortcut
 #SBATCH --output=./outputs/hypertune__bw_fw_MG__shortcut__output.txt
-#SBATCH --error=./errors/hypertune__bw_fw_Mg__shortcut__error.txt
+#SBATCH --error=./errors/hypertune__bw_fw_MG__shortcut__error.txt
 #SBATCH --ntasks=1
 #SBATCH --time=10000
 #SBATCH --mem=100Gb
