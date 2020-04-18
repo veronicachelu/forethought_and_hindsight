d7784e763fd3806a52480bc3f3dd89ee6da370d1
Your Name <you@example.com>
2020-04-18 15:16:07 -0400

    changing configs

--- configs/cartpole_config.py
+++ configs/cartpole_config.py
@@ -28,14 +28,14 @@ volatile_agent_config = {
         "lr_m": [0]
     },
     "bw": {
-        "planning_depth": [1, 4, 8],
+        "planning_depth": [1, 2, 3, 4],
         "replay_capacity": [0],
         "lr": [0.01],
         "lr_p": [0.01],
         "lr_m": np.linspace(0.001, 0.07, 10),
     },
     "fw": {
-        "planning_depth": [1, 4, 8],
+        "planning_depth": [1, 2, 3, 4],
         "replay_capacity": [0],
         "lr": [0.01],
         "lr_p": [0.01],
