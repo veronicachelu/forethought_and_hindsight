de73929965dff8348a34a0e78d020ffa9a508fd8
Your Name <you@example.com>
2020-04-19 16:44:30 -0400

    adding some more plotting

--- slurm_scripts/boyan/allbest_boyan.sh
+++ slurm_scripts/boyan/allbest_boyan.sh
@@ -3,8 +3,7 @@
 #SBATCH --output=./outputs/allbest__boyan__output.txt
 #SBATCH --error=./errors/allbest__boyan__error.txt
 #SBATCH --ntasks=1
-#SBATCH --time=10000
-#SBATCH --mem=100Gb
+#SBATCH --time=12:00:00
 
 source ~/.bashrc
 cd ~/jax_models
