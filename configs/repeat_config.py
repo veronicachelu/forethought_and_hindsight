826e9cba20a466751d1de896497ebb248af17f02
Your Name <you@example.com>
2020-04-19 16:19:48 -0400

    adding some more plotting

--- configs/repeat_config.py
+++ configs/repeat_config.py
@@ -24,7 +24,7 @@ volatile_agent_config = {
         "lr_p": [0],
     },
     "bw": {
-        "planning_depth": [1],
+        "planning_depth": [1, 2, 3, 4],
         "replay_capacity": [0],
         "lr": [0.1],
         "lr_p": [0.1],
