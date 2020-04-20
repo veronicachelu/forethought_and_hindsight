c81d44fecb5da10a670604b7f9a2b211cc750e3d
Your Name <you@example.com>
2020-04-20 07:23:34 -0400

    adding some more plotting

--- slurm_scripts/cartpole/hypertune_all.sh
+++ slurm_scripts/cartpole/hypertune_all.sh
@@ -3,8 +3,7 @@
 #SBATCH --output=./outputs/hypertune__all__cartpole__output.txt
 #SBATCH --error=./errors/hypertune__all__cartpole__error.txt
 #SBATCH --ntasks=1
-#SBATCH --time=10000
-#SBATCH --mem=100Gb
+#SBATCH --time=12:00:00
 #SBATCH --gres=gpu:1
 
 source ~/.bashrc
