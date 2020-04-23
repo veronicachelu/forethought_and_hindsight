672f9b5b549fe635c11f7d8977572e3f0fec8965
Your Name <you@example.com>
2020-04-23 07:13:52 -0400

    latest tweeks

--- slurm_scripts/boyan/hypertune_bw_intr_boyan.sh
+++ slurm_scripts/boyan/hypertune_bw_intr_boyan.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=hypertune__bw__boyan
-#SBATCH --output=./outputs/hypertune__bw__boyan__output.txt
-#SBATCH --error=./errors/hypertune__bw__boyan__error.txt
+#SBATCH --job-name=hypertune__bw_intr__boyan
+#SBATCH --output=./outputs/hypertune__bw_intr__boyan__output.txt
+#SBATCH --error=./errors/hypertune__bw_intr__boyan__error.txt
 #SBATCH --ntasks=1
 #SBATCH --time=12:00:00
 
 source ~/.bashrc
 cd ~/jax_models
-python hyper_tune.py --agent=bw --env=boyan
+python hyper_tune.py --agent=bw_intr --env=boyan
--- slurm_scripts/boyan/hypertune_bw_vaware_boyan.sh
+++ slurm_scripts/boyan/hypertune_bw_vaware_boyan.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=hypertune__bw__boyan
-#SBATCH --output=./outputs/hypertune__bw__boyan__output.txt
-#SBATCH --error=./errors/hypertune__bw__boyan__error.txt
+#SBATCH --job-name=hypertune__bw_vaware__boyan
+#SBATCH --output=./outputs/hypertune__bw_vaware__boyan__output.txt
+#SBATCH --error=./errors/hypertune__bw_vaware__boyan__error.txt
 #SBATCH --ntasks=1
 #SBATCH --time=12:00:00
 
 source ~/.bashrc
 cd ~/jax_models
-python hyper_tune.py --agent=bw --env=boyan
+python hyper_tune.py --agent=bw_vaware --env=boyan
