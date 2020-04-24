6e146053a26a536e9931d435db3c93fe2824ddb1
Your Name <you@example.com>
2020-04-24 15:50:18 -0400

    fixeing bugs for cartpole

--- slurm_scripts/boyan/hypertune_fw_boyan.sh
+++ slurm_scripts/boyan/hypertune_fw_boyan.sh
@@ -4,6 +4,8 @@
 #SBATCH --error=./errors/hypertune__fw__boyan__error.txt
 #SBATCH --ntasks=1
 #SBATCH --time=12:00:00
+#SBATCH --gres=gpu:1
+
 
 source ~/.bashrc
 cd ~/jax_models
--- slurm_scripts/boyan/hypertune_fw_intr_boyan.sh
+++ slurm_scripts/boyan/hypertune_fw_intr_boyan.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=hypertune__bw_intr__boyan
+#SBATCH --job-name=hypertune__fw_intr__boyan
 #SBATCH --output=./outputs/hypertune__bw_intr__boyan__output.txt
-#SBATCH --error=./errors/hypertune__bw_intr__boyan__error.txt
+#SBATCH --error=./errors/hypertune__fw_intr__boyan__error.txt
 #SBATCH --ntasks=1
 #SBATCH --time=12:00:00
 #SBATCH --gres=gpu:1
 
 source ~/.bashrc
 cd ~/jax_models
-python hyper_tune.py --agent=bw_intr --env=boyan
+python hyper_tune.py --agent=fw_intr --env=boyan
