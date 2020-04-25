15c4497386bf65b02e6f9daf07d528f7600b656b
Your Name <you@example.com>
2020-04-25 00:05:43 -0400

    fixeing bugs for cartpole

--- control_experiment.py
+++ control_experiment.py
@@ -1,7 +1,7 @@
 import dm_env
 import tensorflow as tf
 
-from agents import Agent
+from agents.agent import Agent
 from utils.visualizer import *
 
 def run_episodic(agent: Agent,
@@ -47,5 +47,35 @@ def run_episodic(agent: Agent,
             agent.writer.flush()
 
     agent.save_model()
+
+    return np.mean(ep_steps), np.mean(ep_rewards)
+
+def test_agent(agent, environment, num_episodes, max_len):
+    cumulative_reward = 0
+    ep_steps = []
+    ep_rewards = []
+    for episode in np.arange(start=0, stop=num_episodes):
+        # Run an episode.
+        rewards = 0
+        ep_reward = 0
+        timestep = environment.reset()
+        for t in range(max_len):
+            if not agent:
+                action = environment.action_spec().generate_value()
+            else:
+                action = agent.policy(timestep, eval=True)
+            new_timestep = environment.step(action)
+
+            rewards += new_timestep.reward
+            ep_reward += new_timestep.reward
+            if new_timestep.last():
+                break
+
+            timestep = new_timestep
+
+        cumulative_reward += rewards
+
+        ep_steps.append(t)
+        ep_rewards.append(ep_reward)
 
     return np.mean(ep_steps), np.mean(ep_rewards)
