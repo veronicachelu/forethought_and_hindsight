8c01f5d174856597b4207f249467f4c67f6531ac
Your Name <you@example.com>
2020-04-26 14:33:37 -0400

    small changes

--- configs/linear_maze_config.py
+++ configs/linear_maze_config.py
@@ -22,7 +22,8 @@ volatile_agent_config = {
         "replay_capacity": [0],
         "lr": np.linspace(0.1, 1.0, 10),
         "lr_p": [0],
-        "lr_m": [0]
+        "lr_m": [0],
+        "lr_ctrl": 0.4
     },
     "bw": {
         "planning_depth": [1, 4, 8],
@@ -30,6 +31,7 @@ volatile_agent_config = {
         "lr": [0.01],
         "lr_p": [0.01],
         "lr_m": np.linspace(0.001, 0.1, 10),
+        "lr_ctrl": 0.4
     },
     "bw_vaware": {
         "planning_depth": [1],
@@ -37,6 +39,7 @@ volatile_agent_config = {
         "lr": [0.01],
         "lr_p": [0.01],
         "lr_m": np.linspace(0.0001, 0.01, 10),
+        "lr_ctrl": 0.4
     },
     "bw_meta": {
         "planning_depth": [1],
@@ -44,6 +47,7 @@ volatile_agent_config = {
         "lr": [0.01],
         "lr_p": [0.01],
         "lr_m": np.linspace(0.0001, 0.01, 10),
+        "lr_ctrl": 0.4
     },
      "bw_intr": {
         "planning_depth": [1],
@@ -51,6 +55,7 @@ volatile_agent_config = {
         "lr": [0.08],
         "lr_p": [0.08],
         "lr_m": np.linspace(0.001, 0.1, 10),
+         "lr_ctrl": 0.4
     },
     "fw": {
         "planning_depth": [1],
@@ -58,6 +63,7 @@ volatile_agent_config = {
         "lr": [0.01],
         "lr_p": [0.01],
         "lr_m": np.linspace(0.01, 1.0, 10),
+        "lr_ctrl": 0.4
     },
     "fw_rnd": {
         "planning_depth": [1],
@@ -65,6 +71,7 @@ volatile_agent_config = {
         "lr": [0.01],
         "lr_p": [0.01],
         "lr_m": np.linspace(0.01, 1.0, 10),
+        "lr_ctrl": 0.4
     },
     # "ac_vanilla": {
     #     "planning_depth": [0],
