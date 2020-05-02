bcd3072e829dbc639400dfa694d4c0665f440556
Your Name <you@example.com>
2020-05-02 18:08:21 -0400

    adding modified planning

--- run.py
+++ run.py
@@ -10,8 +10,8 @@ import utils
 from run_utils import *
 from utils import *
 
-flags.DEFINE_string('agent', 'vanilla', 'what agent to run')
-flags.DEFINE_string('env', 'stoch_obstacle', 'env')
+flags.DEFINE_string('agent', 'bw_meta', 'what agent to run')
+flags.DEFINE_string('env', 'random_linear', 'env')
 # flags.DEFINE_string('env', 'split', 'env')
 flags.DEFINE_string('logs', str((os.environ['LOGS'])), 'where to save results')
 flags.DEFINE_integer('log_period', 1, 'Log summaries every .... episodes.')
@@ -19,7 +19,7 @@ flags.DEFINE_integer('max_len', 100000, 'Maximum number of time steps an episode
 flags.DEFINE_integer('num_hidden_layers', 0, 'number of hidden layers')
 flags.DEFINE_integer('planning_iter', 1, 'Number of minibatches of model-based backups to run for planning')
 flags.DEFINE_integer('planning_period', 1, 'Number of timesteps of real experience to see before running planning')
-flags.DEFINE_integer('planning_depth', 0, 'Planning depth')
+flags.DEFINE_integer('planning_depth', 1, 'Planning depth')
 flags.DEFINE_integer('replay_capacity', 0, 'Replay capacity')
 flags.DEFINE_integer('model_learning_period', 1,
                      'Number of steps timesteps of real experience to cache before updating the model')
@@ -27,17 +27,17 @@ flags.DEFINE_integer('batch_size', 1, 'size of batches sampled from replay')
 flags.DEFINE_float('discount', .99, 'discounting on the agent side')
 # flags.DEFINE_float('discount', .95, 'discounting on the agent side')
 flags.DEFINE_integer('min_replay_size', 1, 'min replay size before training.')
-flags.DEFINE_float('lr', 0.01, 'learning rate for q optimizer')
+flags.DEFINE_float('lr', 0.1, 'learning rate for q optimizer')
 # flags.DEFINE_float('lr', 0.06, 'learning rate for q optimizer')
 flags.DEFINE_float('lr_ctrl', 0.4, 'learning rate for q optimizer')
-flags.DEFINE_float('lr_p', 0.01, 'learning rate for q optimizer')
+flags.DEFINE_float('lr_p', 0.1, 'learning rate for q optimizer')
 # flags.DEFINE_float('lr_p', 0.06, 'learning rate for q optimizer')
 # flags.DEFINE_float('lr_m',  0.005, 'learning rate for model optimizer')
 # flags.DEFINE_float('lr_m',  0.01, 'learning rate for model optimizer')
 # flags.DEFINE_float('lr_m',  0.005, 'learning rate for model optimizer')
 # flags.DEFINE_float('lr_m',  0.04, 'learning rate for model optimizer')
 # flags.DEFINE_float('lr_m',  0.02, 'learning rate for model optimizer')
-flags.DEFINE_float('lr_m',  0.01, 'learning rate for model optimizer')
+flags.DEFINE_float('lr_m',  0.5, 'learning rate for model optimizer')
 
 FLAGS = flags.FLAGS
 
