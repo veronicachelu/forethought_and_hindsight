5ed1d69be977b22030badedf4045c929ca0d86ce
Your Name <you@example.com>
2020-05-05 20:43:18 -0400

    adding modified planning

---
+++ slurm_scripts/reg2_obstacle/allbest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=allbest__reg2_obstacle
+#SBATCH --output=./outputs/allbest__reg2_obstacle__output.txt
+#SBATCH --error=./errors/allbest__reg2_obstacle__error.txt
+#SBATCH --ntasks=1
+#SBATCH --time=6:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8gb
+
+source ~/.bashrc
+cd ~/jax_models
+python run_all_best.py --env=reg2_obstacle
