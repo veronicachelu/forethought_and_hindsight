24236fa6e472c5335615575b355ede2e517459ee
Your Name <you@example.com>
2020-05-05 20:48:59 -0400

    adding modified planning

---
+++ slurm_scripts/reg1_obstacle/run_mb_bw_iter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=allbest__reg1_obstacle_mb_bw_iter
+#SBATCH --output=./outputs/allbest__reg1_obstacle_mb_bw_iter__output.txt
+#SBATCH --error=./errors/allbest__reg1_obstacle_mb_bw_iter__error.txt
+#SBATCH --ntasks=1
+#SBATCH --time=6:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8gb
+
+source ~/.bashrc
+cd ~/jax_models
+python run.py --env=reg1_obstacle --agent=mb_bw_iter --lr=0.001 --lr_p=0.001 --lr_m=0.005 --planning_depth=1
