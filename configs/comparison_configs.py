b803c70016c2d8fb7bab04f7e793f4eff27fb465
Your Name <you@example.com>
2020-05-18 17:17:01 -0400

    fixed bug in fw_paml; fixed project norm in true_bw

--- configs/comparison_configs.py
+++ configs/comparison_configs.py
@@ -656,6 +656,58 @@ configs={
             "replay_capacities": [0, 0, 0, 0, 0, 0]
         },
     },
+    "reg01_obstacle": {
+        "bw_p_fw_p_all": {
+            "agents": ["p_bw",
+                       "p_true_bw",
+                       "p_fw",
+                       "p_fw_PAML",
+                       "p_bw_PAML"
+                       ],
+            "planning_depths": [1, 1, 1, 1, 1, 1],
+            "replay_capacities": [0, 0, 0, 0, 0, 0]
+        },
+        "bw_c_fw_p_all": {
+            "agents": ["c_bw",
+                       "c_true_bw",
+                       "p_fw",
+                       "p_fw_PAML",
+                       "c_bw_PAML"
+                       ],
+            "planning_depths": [1, 1, 1, 1, 1, 1],
+            "replay_capacities": [0, 0, 0, 0, 0, 0]
+        },
+        "bw_c_bw_p_all": {
+            "agents": ["p_bw",
+                       "c_bw",
+                       "p_true_bw",
+                       "c_true_bw",
+                       "p_bw_PAML",
+                       "c_bw_PAML"
+                       ],
+            "planning_depths": [1, 1, 1, 1, 1, 1],
+            "replay_capacities": [0, 0, 0, 0, 0, 0]
+        },
+        "fw_c_fw_p_all": {
+            "agents": ["p_fw",
+                       "c_fw",
+                       "p_fw_PAML",
+                       "c_fw_PAML"
+                       ],
+            "planning_depths": [1, 1, 1, 1, 1, 1],
+            "replay_capacities": [0, 0, 0, 0, 0, 0]
+        },
+        "mb_bw_c_fw_p_all": {
+            "agents": ["mb_c_bw",
+                       "mb_c_true_bw",
+                       "mb_p_fw",
+                       "mb_p_fw_PAML",
+                       "mb_c_bw_PAML"
+                       ],
+            "planning_depths": [1, 1, 1, 1, 1, 1],
+            "replay_capacities": [0, 0, 0, 0, 0, 0]
+        },
+    },
     "bipartite": {
         "MLE_PAML_all_mle": {
             "agents": ["c_bw_MLE",
