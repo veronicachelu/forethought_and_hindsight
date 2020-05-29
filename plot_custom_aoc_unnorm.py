9be42f03b1e56a07053de3c97afdf179b0960d21
Your Name <you@example.com>
2020-05-29 17:54:06 -0400

    plotting

--- plot_custom_aoc_unnorm.py
+++ plot_custom_aoc_unnorm.py
@@ -347,7 +347,7 @@ def main(argv):
     fig.tight_layout()
     fig.subplots_adjust(right=0.90)
     fig.savefig(os.path.join(plots_dir,
-                             "{}_{}.png".format("aoc",
+                             "{}_{}.png".format("aoc_unnorm",
                                                 FLAGS.plotting)),
                 bbox_inches='tight',
                 )
