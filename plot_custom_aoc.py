2da73f61003ac88b0f3e3d21d21f961bd8b4a89a
Your Name <you@example.com>
2020-06-02 00:04:25 -0400

    plotting

--- plot_custom_aoc.py
+++ plot_custom_aoc.py
@@ -345,7 +345,7 @@ def main(argv):
     )
 
     fig.tight_layout()
-    fig.subplots_adjust(right=0.86)
+    fig.subplots_adjust(right=0.87)
     fig.savefig(os.path.join(plots_dir,
                              "{}_{}.png".format("aoc",
                                                 FLAGS.plotting)),
